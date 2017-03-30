25f96d4afaeceda02df6fd006e1772699e6296b8
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-03-30 12:01:33 -0500

    before review

--- client/clientReact/main.js
+++ client/clientReact/main.js
@@ -5,28 +5,82 @@ var FormPart = require('./Form')
 
 var MainComponent = React.createClass({					// iniitial status is blank data, ready to receive data
 	getInitialState: function(){
-		return {data: []}
+		return {loggedIn: false, username: ''}
+			console.log('loggedIn:', loggedIn)
 	},
 
-	componentDidMount: function(){					
+	componentDidMount: function(){						// called after render				
 		var state = this.state;							// these get set the state of main page
 		var self = this;
-
+			console.log('state:',state, ' self:',self)
 		request.get('http://localhost:9393/characters') // get the data to put on main page
 		.end(function(err, data){
 			state.data = data.body;						// the state is now the data you grabbed
-			self.setState(state)						// the new state is set
+			self.setState(state)						// the new state is set render is called
 		})
 	},
 
 	createCharacter: function(character){			
 		var state = this.state;							// this doesn't know about other states
 		var self = this;								// so declare the current state is the starting point 
-		
+			console.log('state:',state, ' self:',self)
 		request.post('http://localhost:9393/characters')
-		.send('character=' + item)						// sends data to post
+		// .type('form') use this so u dont need parse
+		// send({title: item})
+		console.log(data.body)
+		// state.data = data.body;
+		.send('character=' + item)	//don't need		// sends data to post
 		.end(function(err, data){
 		})
+	},
+
+	getLoginInfo: function(isLoggedIn, username){		// validates user is logged in 
+		console.log('isLoggedIn:',isLoggedIn, username)
+		var state = this.state;							// declare current state so it can be saved
+		state.loggedIn = isLoggedIn;					// set state to logged in
+		state.username = username;						// set state to username entered
+		state.mainPage = true 							// allow user to proceeed to main
+		this.setState(state)							// sets the new state 
+	},
+})	
+
+var UserLogin = React.createClass({					// component to handle pseudo-login form
+	getInitialState: function() {					// init ready to enter a username
+		return {username: ''}						
+			console.log('ready for username')
+	},
+
+	handleInput: function(event){					// allows you to type into form
+		var state = this.state;
+		
+		state.username = event.target.value; 
+		this.setState(state) 
+	},
+
+	handleLogin: function(event){					//	validate login so you can send that change in state up
+		this.props.getLoginInfo(true, this.state.username)
+	},
+
+	render: function(){								// render the login on the main
+		return(
+			<div>
+				<form>
+					<input onChange={this.handleInput} type="text" name="username" placeholder="Username"/>
+					<button onClick={this.handleLogin}>Submit</button>
+				</form>
+			</div>
+		)
+	},
+
+	updateValue: function(){
+		this.setState
+	}
+})
+
+var MainPage = React.createClass({
+	getInitialState: function(){
+		return {characters: []}
+		console.log('ready for characters')
 	},
 
 	render: function(){
@@ -43,11 +97,22 @@ var MainComponent = React.createClass({					// iniitial status is blank data, re
 			</div>
 		)
 
+		var characters = this.props.characters.map(function(character, i){
+			return (
+				<div>
+					<h3>Cartoon Characters and Shows</h3>
+						return(
+							<li keys={i}> {character.name} is a character on {character.cartoon}</li>
+						)	
+				</div>
+			)
+		})
 	}
-});
+})
+
 
 //This renders the shadow DOM items that changed to appear like on the real DOM
-React.DOM.render(
+ReactDOM.render(
 	<MainComponent/>, document.getElementById('container')
 )
 
--- client/scripts/build/build.js
+++ client/scripts/build/build.js
@@ -50,26 +50,97 @@ var MainComponent = React.createClass({
 	displayName: 'MainComponent',
 	// iniitial status is blank data, ready to receive data
 	getInitialState: function () {
-		return { data: [] };
+		return { loggedIn: false, username: '' };
+		console.log('loggedIn:', loggedIn);
 	},
 
 	componentDidMount: function () {
+		// called after render				
 		var state = this.state; // these get set the state of main page
 		var self = this;
-
+		console.log('state:', state, ' self:', self);
 		request.get('http://localhost:9393/characters') // get the data to put on main page
 		.end(function (err, data) {
 			state.data = data.body; // the state is now the data you grabbed
-			self.setState(state); // the new state is set
+			self.setState(state); // the new state is set render is called
 		});
 	},
 
 	createCharacter: function (character) {
 		var state = this.state; // this doesn't know about other states
 		var self = this; // so declare the current state is the starting point 
-
-		request.post('http://localhost:9393/characters').send('character=' + item) // sends data to post
+		console.log('state:', state, ' self:', self);
+		request.post('http://localhost:9393/characters');
+		// .type('form') use this so u dont need parse
+		// send({title: item})
+		console.log(data.body)
+		// state.data = data.body;
+		.send('character=' + item) //don't need		// sends data to post
 		.end(function (err, data) {});
+	},
+
+	getLoginInfo: function (isLoggedIn, username) {
+		// validates user is logged in 
+		console.log('isLoggedIn:', isLoggedIn, username);
+		var state = this.state; // declare current state so it can be saved
+		state.loggedIn = isLoggedIn; // set state to logged in
+		state.username = username; // set state to username entered
+		state.mainPage = true; // allow user to proceeed to main
+		this.setState(state); // sets the new state 
+	}
+});
+
+var UserLogin = React.createClass({
+	displayName: 'UserLogin',
+	// component to handle pseudo-login form
+	getInitialState: function () {
+		// init ready to enter a username
+		return { username: '' };
+		console.log('ready for username');
+	},
+
+	handleInput: function (event) {
+		// allows you to type into form
+		var state = this.state;
+
+		state.username = event.target.value;
+		this.setState(state);
+	},
+
+	handleLogin: function (event) {
+		//	validate login so you can send that change in state up
+		this.props.getLoginInfo(true, this.state.username);
+	},
+
+	render: function () {
+		// render the login on the main
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(
+				'form',
+				null,
+				React.createElement('input', { onChange: this.handleInput, type: 'text', name: 'username', placeholder: 'Username' }),
+				React.createElement(
+					'button',
+					{ onClick: this.handleLogin },
+					'Submit'
+				)
+			)
+		);
+	},
+
+	updateValue: function () {
+		this.setState;
+	}
+});
+
+var MainPage = React.createClass({
+	displayName: 'MainPage',
+
+	getInitialState: function () {
+		return { characters: [] };
+		console.log('ready for characters');
 	},
 
 	render: function () {
@@ -92,11 +163,33 @@ var MainComponent = React.createClass({
 				})
 			)
 		);
+
+		var characters = this.props.characters.map(function (character, i) {
+			return React.createElement(
+				'div',
+				null,
+				React.createElement(
+					'h3',
+					null,
+					'Cartoon Characters and Shows'
+				),
+				'return(',
+				React.createElement(
+					'li',
+					{ keys: i },
+					' ',
+					character.name,
+					' is a character on ',
+					character.cartoon
+				),
+				')'
+			);
+		});
 	}
 });
 
 //This renders the shadow DOM items that changed to appear like on the real DOM
-React.DOM.render(React.createElement(MainComponent, null), document.getElementById('container'));
+ReactDOM.render(React.createElement(MainComponent, null), document.getElementById('container'));
 
 },{"./Form":1,"react":180,"react-dom":29,"superagent":181}],3:[function(require,module,exports){
 
